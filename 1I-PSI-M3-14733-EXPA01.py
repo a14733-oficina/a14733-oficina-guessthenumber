6336346c93ec5e5777e8e7b74e06a4b95a8de9fa
a14733-oficina <a14733@oficina.pt>
2024-12-17 10:05:38 +0000

    Update 1I-PSI-M3-14733-EXPA01.py

--- 1I-PSI-M3-14733-EXPA01.py
+++ 1I-PSI-M3-14733-EXPA01.py
@@ -1,16 +1,16 @@
 import random
 nome =str(input("Insira o seu nome: "))
 print("Tem 6 tentativas")
-aleatorio = random.randint(1,21)
+aleatorio = random.randint(1,21)#escolhe um  numero de 1 a 20 
 tentativas = 6
 while tentativas > 0:
     num = int(input("Insira o seu numero: "))
     if num == aleatorio:
         print("Acertas-te")
-        exit()
+        exit()#caso o user acerte sai do programa 
     elif num > aleatorio:
         print("Numero incorreto, inferior")
-        tentativas -=1
+        tentativas -=1#menos 1 tentaviva das 6 q tinha
         print(f"Tens {tentativas} tentativas.")
     elif num < aleatorio:
         print("Numero incorreto, superior")
